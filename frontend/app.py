685ed012ad93646b7305f5beac43d63dc026b924
TejeshKumarReddyC <tejeshchagantireddy@gmail.com>
2025-06-14 21:12:13 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -19,5 +19,5 @@ def index():
     return render_template_string(TEMPLATE, messages=res.json())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
 
